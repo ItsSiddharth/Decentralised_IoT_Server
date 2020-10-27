d76c48bbd85728990dfc6f7a85d5c04861220dd6
ItsSiddharth <siddharth.menon2018@vitstudent.ac.in>
2020-10-27 18:47:16 +0530

    Subplots enabled

--- Decentralised_App/routes.py
+++ Decentralised_App/routes.py
@@ -16,12 +16,15 @@ from models import Field
 
 
 def create_figure():
-	field = Field.query.filter_by(number=1).first()
-	fig = Figure()
-	axis = fig.add_subplot(1, 1, 1)
-	xs = [x for x in range(len(field.datapoints))]
-	ys = field.datapoints
-	axis.plot(xs, ys)
+	no_of_fields = db.session.query(Field).count()
+	fig, axs = plt.subplots(no_of_fields)
+	for i in range(no_of_fields):
+		field = Field.query.filter_by(number=i+1).first()
+		xs = [x for x in range(len(field.datapoints))]
+		ys = field.datapoints
+		axs[i].plot(xs, ys, '--bo')
+		axs[i].set_title(f"Field {i+1}")
+	fig.tight_layout(pad=3.0)
 	return fig
 
 @app.route("/", methods=["GET", "POST"])
